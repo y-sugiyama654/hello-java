69a14e192c6e334aed8a135cc15b12a00e46f938
Yuta <y.sugiyama54@gmail.com>
2020-10-12 23:30:31 +0900

    文字列の出力

---
+++ server/src/Greeting.java
@@ -0,0 +1,9 @@
+class Greeting
+{
+    public static void main(String args[])
+    {
+        System.out.println("Good morning");
+        System.out.println("Good afternoon");
+        System.out.println("Good evening");
+    }
+} 
