9427d899b67b6291ab1d1e916ffb864397cdc39e
Yuta <y.sugiyama54@gmail.com>
2020-10-12 23:32:18 +0900

    変数の出力

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -2,8 +2,11 @@ class Greeting
 {
     public static void main(String args[])
     {
-        System.out.println("Good morning");
-        System.out.println("Good afternoon");
-        System.out.println("Good evening");
+        // System.out.println("Good morning");
+        // System.out.println("Good afternoon");
+        // System.out.println("Good evening");
+
+        int var = 1;
+        System.out.println(var);
     }
-} 
+}
