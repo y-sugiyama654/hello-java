622a518d7d2bff446d79b19f0a68d093db0188da
Yuta <y.sugiyama54@gmail.com>
2020-10-12 23:35:09 +0900

    変数名のルール

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -6,7 +6,17 @@ class Greeting
         // System.out.println("Good afternoon");
         // System.out.println("Good evening");
 
+        // int var = 1;
+        // System.out.println(var);
+
         int var = 1;
+        int var01 = 2;
+        int var_01 = 3;
+        int var$01 = 4;
         System.out.println(var);
+        System.out.println(var01);
+        System.out.println(var_01);
+        System.out.println(var$01);
+
     }
 }
