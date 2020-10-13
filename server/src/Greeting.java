0f3032a31c8a660f5e8656755eb9584b71fff133
Yuta <y.sugiyama54@gmail.com>
2020-10-13 23:48:43 +0900

    小数のデータ型

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -2,13 +2,16 @@ class Greeting
 {
     public static void main(String args[])
     {
+        // 文字列の出力
         // System.out.println("Good morning");
         // System.out.println("Good afternoon");
         // System.out.println("Good evening");
 
+        // 変数の出力
         // int var = 1;
         // System.out.println(var);
 
+        // 変数名のルール
         // int var = 1;
         // int var01 = 2;
         // int var_01 = 3;
@@ -19,13 +22,19 @@ class Greeting
         // System.out.println(var$01);
 
         // 整数のデータ型
-        byte var01 = 1;
-        short var02 = 12345;
-        int var03 = 1234567890;
-        long var04 = 123456789000L;
+        // byte var01 = 1;
+        // short var02 = 12345;
+        // int var03 = 1234567890;
+        // long var04 = 123456789000L;
+        // System.out.println(var01);
+        // System.out.println(var02);
+        // System.out.println(var03);
+        // System.out.println(var04);
+
+        // 小数のデータ型
+        double var01 = 1.23456789;
+        float var02 = 1.234F;
         System.out.println(var01);
         System.out.println(var02);
-        System.out.println(var03);
-        System.out.println(var04);
     }
 }
