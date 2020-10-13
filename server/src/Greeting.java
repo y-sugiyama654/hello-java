f227afafea73f8dd77b9564b9c0cad46661b4783
Yuta <y.sugiyama54@gmail.com>
2020-10-13 23:44:07 +0900

    整数のデータ型

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -9,14 +9,23 @@ class Greeting
         // int var = 1;
         // System.out.println(var);
 
-        int var = 1;
-        int var01 = 2;
-        int var_01 = 3;
-        int var$01 = 4;
-        System.out.println(var);
-        System.out.println(var01);
-        System.out.println(var_01);
-        System.out.println(var$01);
+        // int var = 1;
+        // int var01 = 2;
+        // int var_01 = 3;
+        // int var$01 = 4;
+        // System.out.println(var);
+        // System.out.println(var01);
+        // System.out.println(var_01);
+        // System.out.println(var$01);
 
+        // 整数のデータ型
+        byte var01 = 1;
+        short var02 = 12345;
+        int var03 = 1234567890;
+        long var04 = 123456789000L;
+        System.out.println(var01);
+        System.out.println(var02);
+        System.out.println(var03);
+        System.out.println(var04);
     }
 }
