90d979a76e480ec510b543b8ed7d1a517af38590
Yuta <y.sugiyama54@gmail.com>
2020-10-15 22:20:00 +0900

    二次元配列2

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -79,13 +79,21 @@ class Greeting
         // System.out.println(arr[2]);
 
         // 二次元配列
-        String[][] arr;
-        arr = new String[2][2];
+        // String[][] arr;
+        // arr = new String[2][2];
 
-        arr[0][0] = "sato";
-        arr[0][1] = "suziki";
-        arr[1][0] = "takahashi";
-        arr[1][1] = "tanaka";
+        // arr[0][0] = "sato";
+        // arr[0][1] = "suziki";
+        // arr[1][0] = "takahashi";
+        // arr[1][1] = "tanaka";
+
+        // System.out.println(arr[0][0]);
+        // System.out.println(arr[0][1]);
+        // System.out.println(arr[1][0]);
+        // System.out.println(arr[1][1]);
+
+        // 二次元配列2
+        String[][] arr = {{"sato", "suzuki"}, {"takahashi", "tanaka"}};
 
         System.out.println(arr[0][0]);
         System.out.println(arr[0][1]);
