534509cfbc5f76d4285fd0229fe693b0b218e94c
Yuta <y.sugiyama54@gmail.com>
2020-10-15 22:18:15 +0900

    二次元配列

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -70,12 +70,26 @@ class Greeting
         // System.out.println(arr[2]);
 
         // 配列内の値の置換
-        String[] arr = {"sato", "suzuki", "takahashi"};
+        // String[] arr = {"sato", "suzuki", "takahashi"};
 
-        arr[1] = "tanaka";
+        // arr[1] = "tanaka";
 
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+
+        // 二次元配列
+        String[][] arr;
+        arr = new String[2][2];
+
+        arr[0][0] = "sato";
+        arr[0][1] = "suziki";
+        arr[1][0] = "takahashi";
+        arr[1][1] = "tanaka";
+
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[1][0]);
+        System.out.println(arr[1][1]);
     }
 }
