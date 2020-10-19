ec23023169eaf3ee286f914d10223cdb29f5ec56
Yuta <y.sugiyama54@gmail.com>
2020-10-19 23:39:11 +0900

    代入演算子

--- server/src/Greeting.java
+++ server/src/Greeting.java
@@ -130,10 +130,18 @@ class Greeting
         // System.out.println(y >= 5 && y <= 10);
 
         // 論理演算子2
-        int x = 8;
-        int y = 3;
+        // int x = 8;
+        // int y = 3;
 
-        System.out.println(x >= 5 || x <= 10);
-        System.out.println(y >= 5 || y <= 10);
+        // System.out.println(x >= 5 || x <= 10);
+        // System.out.println(y >= 5 || y <= 10);
+
+        // 代入演算子
+        int x = 8;
+        int y = 12;
+        int z = 20;
+
+        System.out.println(x += 10);
+        System.out.println(z += y);
     }
 }
